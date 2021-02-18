cd1b6383d7a9601cd0cbf0bc44f97bde0377b75a
Yerassyl Arkhat <190103017@mail.ru>
2021-02-18 23:53:30 +0600

    add home&about pages

--- lab5/resources/views/about.blade.php
+++ lab5/resources/views/about.blade.php
@@ -1,9 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>lab5 about</title>
+	<title>about</title>
+	
+<link rel="stylesheet" type="text/css" href="aboutstyle.css"><link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"><link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+<style>
+	*{background-color: #f8f1f1;
+	margin: 0 auto;
+	padding: 0;
+}header{width: 100%;height: 120px;display: flex;flex-wrap: wrap;
+	justify-content: space-between;
+}.icon{font-family: 'Courgette', cursive;font-size: 30px;
+	margin-top: 30px;margin-left: -100px;}
+header .header_right{font-family: 'Courgette', cursive;display: flex;
+	margin-right: 0;width: 600px;height: 100px;} 
+.links{	font-size: 30px;
+	margin-top: 30px;
+	cursor: pointer;
+}.links:hover{
+	font-size: 35px;
+}.content{	font-family: 'Montserrat', sans-serif;
+	height: 700px;
+	width: 100%;
+	max-width: 1400px;
+	display: flex;
+	justify-content: space-between;
+}.left_content{	margin-top: 50px;}
+.right_content{	margin-left: 30px;text-align: left;margin-top: 50px;
+}.right_content h1{font-weight: 150;font-size: 50px;word-spacing: 30px;
+}.right_content h6{	margin-top: 50px;font-size: 20px;line-height: 1.8;
+}	
+</style>
 </head>
 <body>
-	<h1>about</h1>
+	<header>
+		<div class="header_left">
+			<div class="icon">
+				@earkhat
+			</div>
+		</div>
+		<div class="header_right">
+			<div class="links">Home</div>
+			<div class="links">About</div>
+			<div class="links">Pages</div>
+			<div class="links">Contacts</div>
+		</div>
+	</header>
+
+	<div class="content">
+		<div class="left_content">
+			<img src="about-us.png" style="width: 700px; height: 600px;">
+		</div>
+		<div class="right_content">
+			<h1>
+	          <p>Let me  
+	          Introduce 
+	      		myself</p>
+			</h1>
+			<h6>
+				My name is Yerassyl Arkhat. I am 18 years old. I am from Semey, East Kazakhstan Region.
+				And now I am studying IT at Suleiman Demirel University second year. My hobbies are playing footbal, watching horror movies.  I have a lot of achieves in sports: Boxing and Football. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error rerum iure obcaecati vel possimus officia maiores perferendis ut! Quos, perspiciatis.
+			</h6>
+		</div>
+	</div>
 </body>
 </html>
--- lab5/resources/views/home.blade.php
+++ lab5/resources/views/home.blade.php
@@ -1,28 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lab5</title>
-	<style>
-		*{margin: 0 auto;padding: 0;background-color: #eae3c8;}
-		header .header{
-			width: 100%;
-			max-width: 1200px;
-			height: 100px;
-			display: flex;
-			flex-wrap: wrap;
-			border: 1px solid blue;
-		}
-		.links a{color:#383e56;text-decoration: none;font-size: 35px;}
-		.links{margin-top: 20px;}
-	</style>
+	<title>lab5</title>
+	<link rel="stylesheet" type="text/css" href="style.css"><link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+<style>
+	*{background-color: #f8f1f1;margin: 0 auto;padding: 0;font-family: 'Courgette', cursive;}header{width: 100%;
+	height: 120px;display: flex;flex-wrap: wrap;justify-content: space-between;}.icon{font-size: 30px;
+	margin-top: 30px;margin-left: -100px;}header .header_right{display: flex;margin-right: 0;width: 600px;
+	height: 100px;} .links{font-size: 30px;margin-top: 30px;cursor: pointer;}.links:hover{font-size: 35px;}
+.content{height: 700px;width: 100%;max-width: 1400px;}.top_content{margin-top: 50px;display: flex;justify-content: space-between;}.left_content{font-size: 40px;margin-top: 100px;}.btns{margin-top: 20px;width: 450px;
+	display: flex;margin-left: -20px;}.aboutbtn, .gallerybtn{padding: 14px 38px;background-color: #fff3e6;color: #0d335d;
+  font-size: 20px;border-radius: 5px;}.aboutbtn:hover, .gallerybtn:hover{background-color: transparent;transition: 300ms;
+	padding: 11px 32px;font-size: 23px;
+}
+</style>
 </head>
 <body>
 	<header>
-		<div class="header">
-			<div class="links"><a href="">Main page</a></div>
-			<div class="links"><a href="">About</a></div>
-			<div class="links"><a href="">Gallery</a></div>
+		<div class="header_left">
+			<div class="icon">
+				@earkhat
+			</div>
+		</div>
+		<div class="header_right">
+			<div class="links">Home</div>
+			<div class="links">About</div>
+			<div class="links">Pages</div>
+			<div class="links">Contacts</div>
 		</div>
 	</header>
+
+	<div class="content">
+		<div class="top_content">
+			<div class="left_content">
+				<h3>Hello</h3>
+				<h1>I am Yerassyl</h1>
+				<h4>Second year student SDU</h4>
+				<div class="btns">
+					<button class="aboutbtn">About me</button>
+				<button class="gallerybtn">My gallery</button>
+				</div>
+			</div>
+			<div class="right_content">
+				<img src="banner-image.png" style="width: 600px; height: 600px;">
+			</div>
+		</div>
+	</div>
 </body>
 </html>
