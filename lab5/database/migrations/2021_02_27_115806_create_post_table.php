8688ac3e8aab7253988b5124444fcaea7606dd52
Yerassyl Arkhat <190103017@mail.ru>
2021-02-27 18:06:25 +0600

    renamed migration

--- lab5/database/migrations/2021_02_27_115806_create_post_table.php
+++ lab5/database/migrations/2021_02_27_115806_create_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Post extends Migration
+class CreatePostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class Post extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->title();
+            $table->body();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class Post extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('post');
     }
 }
